04f8b660a9b515fec59db997a41cf3df203272b0
hectorplazas <95445946+hectorplazas@users.noreply.github.com>
2021-12-02 18:11:35 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const server = require('http').Server(app);
 
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '../../dist'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(__dirname+ '/index.html');
+res.sendFile(__dirname+ '../../dist/index.html');
 });
  
 // Start the app by listening on the default Heroku port
