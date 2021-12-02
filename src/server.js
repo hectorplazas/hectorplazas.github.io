a0ead68b1c6a013a787e4982d8ac53e3936a7f2a
hectorplazas <95445946+hectorplazas@users.noreply.github.com>
2021-12-02 17:58:25 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/frontend'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+res.sendFile(__dirname+'/dist/frontend/index.html');
 });
  
 // Start the app by listening on the default Heroku port
